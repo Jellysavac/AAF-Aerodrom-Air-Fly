6112f8d73b43c4aa6205a942fb0e20623bc5d337
kisa97 <sakinagasaki97@gmail.com>
2020-04-05 19:04:01 +0200

    Let Controller importi!

--- AirFlySPRING/src/main/java/airfly/controller/LetController.java
+++ AirFlySPRING/src/main/java/airfly/controller/LetController.java
@@ -3,16 +3,24 @@ package airfly.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import airfly.dto.getFlightsDto;
 import airfly.repository.LetRepository;
 import model.Flight;
 
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/let")
 public class LetController {
 	
+	@Autowired
 	LetRepository lr;
 	
 	@GetMapping("/getAllFlights")
